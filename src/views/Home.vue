c4df36f366e2cceec04a738949dc9c896954a345
duodecimo.fernandes <duodecimo.fernandes@virtualinteractions.com.br>
2020-06-12 09:49:44 -0300

    inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h2>Duó Video.</h2>
+    <br />
+    Este código demonstra a utilização do componente video no vue.js.
+    <br />
+    Pode ser bem mais customizado, mas, por enquanto, achei melhor deixar bem
+    simples.
+    <br />
+    Basta clicar em Video.
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
+  name: "Home"
+  /*
   components: {
     HelloWorld
   }
+*/
 };
 </script>
---
+++ src/views/Video.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>
+      Nada melhor para falar sobre o componente video no vue do que reproduzir
+      um video.
+    </h1>
+    <video controls="true" :poster="require('@/assets/duoVideo.png')">
+      <source src="@/assets/video.webm" type="video/webm" />
+    </video>
+  </div>
+</template>
